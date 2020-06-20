e31d7839d77c739a650567885aeae1f5002c1aa1
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2020-06-20 09:58:18 +0100

    Refactor Ouros SplashActivity

--- app/src/main/java/com/gigabytedevsinc/mezue/ouros/activities/SplashActivity.java
+++ app/src/main/java/com/gigabytedevsinc/mezue/ouros/activities/SplashActivity.java
@@ -1,15 +1,31 @@
 package com.gigabytedevsinc.mezue.ouros.activities;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
 
-import com.dm.material.dashboard.candybar.activities.CandyBarSplashActivity;
+import com.gigabytedevsinc.mezue.ouros.R;
 
+import candybar.lib.activities.CandyBarSplashActivity;
+import candybar.lib.activities.configurations.SplashScreenConfiguration;
+
+/**
+ * Project - Ouros
+ * Created with Android Studio
+ * Company: Gigabyte Developers
+ * User: Emmanuel Nwokoma
+ * Title: Founder and CEO
+ * Day: Saturday, 20
+ * Month: June
+ * Year: 2020
+ * Date: 20 Jun, 2020
+ * Time: 7:57 AM
+ * Desc: SplashActivity
+ **/
 public class SplashActivity extends CandyBarSplashActivity {
 
+    @NonNull
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        initSplashActivity(savedInstanceState, MainActivity.class);
+    public SplashScreenConfiguration onInit() {
+        return new SplashScreenConfiguration(MainActivity.class)
+                .setBottomText(getString(R.string.splash_screen_title));
     }
 }
