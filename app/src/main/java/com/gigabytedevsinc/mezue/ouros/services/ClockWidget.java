dba8f71abe2ed5894c72f5a4bdd79e86f8ada97e
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2020-06-20 19:45:21 +0100

    Added ClockWidget feature

---
+++ app/src/main/java/com/gigabytedevsinc/mezue/ouros/services/ClockWidget.java
@@ -0,0 +1,18 @@
+package com.gigabytedevsinc.mezue.ouros.services;
+
+import candybar.lib.services.CandyBarWidgetService;
+
+/**
+ * Project - Ouros
+ * Created with Android Studio
+ * Company: Gigabyte Developers
+ * User: Emmanuel Nwokoma
+ * Title: Founder and CEO
+ * Day: Saturday, 20
+ * Month: June
+ * Year: 2020
+ * Date: 20 Jun, 2020
+ * Time: 8:57 AM
+ * Desc: ClockWidget
+ **/
+public class ClockWidget extends CandyBarWidgetService {}
