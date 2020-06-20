7205617030f7ec3f6f95b8366faf440783f1e8b1
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2020-06-20 09:59:40 +0100

    Refactor Ouros CandyBar Application class to work with new Engine

--- app/src/main/java/com/gigabytedevsinc/mezue/ouros/applications/CandyBar.java
+++ app/src/main/java/com/gigabytedevsinc/mezue/ouros/applications/CandyBar.java
@@ -1,20 +1,34 @@
 package com.gigabytedevsinc.mezue.ouros.applications;
 
-import com.dm.material.dashboard.candybar.applications.CandyBarApplication;
+import androidx.annotation.NonNull;
 
+import candybar.lib.applications.CandyBarApplication;
+
+/**
+ * Project - Ouros
+ * Created with Android Studio
+ * Company: Gigabyte Developers
+ * User: Emmanuel Nwokoma
+ * Title: Founder and CEO
+ * Day: Saturday, 20
+ * Month: June
+ * Year: 2020
+ * Date: 20 Jun, 2020
+ * Time: 8:30 AM
+ * Desc: CandyBar
+ **/
 public class CandyBar extends CandyBarApplication {
 
+    @NonNull
     @Override
-    public void onCreate() {
-        //Sample configuration
-        super.onCreate();
+    public Configuration onInit() {
+        // Sample configuration
         Configuration configuration = new Configuration();
 
         configuration.setGenerateAppFilter(true);
         configuration.setGenerateAppMap(true);
         configuration.setGenerateThemeResources(true);
-        configuration.setDashboardThemingEnabled(true);
 
-        initApplication(configuration);
+        return configuration;
     }
 }
