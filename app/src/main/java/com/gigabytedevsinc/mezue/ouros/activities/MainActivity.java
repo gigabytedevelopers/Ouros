dca7608517f09dd6eb088d33925aa9e6d5516b32
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2020-06-20 09:56:48 +0100

    Refactor Ourus MainActivity

--- app/src/main/java/com/gigabytedevsinc/mezue/ouros/activities/MainActivity.java
+++ app/src/main/java/com/gigabytedevsinc/mezue/ouros/activities/MainActivity.java
@@ -1,28 +1,35 @@
 package com.gigabytedevsinc.mezue.ouros.activities;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
 
-import com.dm.material.dashboard.candybar.activities.CandyBarMainActivity;
-import com.dm.material.dashboard.candybar.helpers.InAppBillingHelper;
-import com.gigabytedevsinc.mezue.ouros.R;
 import com.gigabytedevsinc.mezue.ouros.licenses.License;
 
+import candybar.lib.activities.CandyBarMainActivity;
+import candybar.lib.activities.configurations.ActivityConfiguration;
+
+/**
+ * Project - Ouros
+ * Created with Android Studio
+ * Company: Gigabyte Developers
+ * User: Emmanuel Nwokoma
+ * Title: Founder and CEO
+ * Day: Saturday, 20
+ * Month: June
+ * Year: 2020
+ * Date: 20 Jun, 2020
+ * Time: 8:00 AM
+ * Desc: MainActivity
+ **/
 public class MainActivity extends CandyBarMainActivity {
 
-
+    @NonNull
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        initMainActivity(savedInstanceState,
-                new InAppBillingHelper.Property(
-                        License.isLicenseCheckerEnabled(),
-                        License.getRandomString(),
-                        License.getLicenseKey(),
-                        License.getDonationProductsId(),
-                        License.getPremiumRequestProductsId(),
-                        License.getPremiumRequestProductsCount()));
-
+    public ActivityConfiguration onInit() {
+        return new ActivityConfiguration()
+                .setLicenseCheckerEnabled(License.isLicenseCheckerEnabled())
+                .setLicenseKey(License.getLicenseKey())
+                .setRandomString(License.getRandomString())
+                .setDonationProductsId(License.getDonationProductsId())
+                .setPremiumRequestProducts(License.getPremiumRequestProductsId(), License.getPremiumRequestProductsCount());
     }
-
 }
